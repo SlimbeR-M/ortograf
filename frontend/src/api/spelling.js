8ce7a8b893fdd9203e118b7a71f91cc1fb430854
Jonathan Hernández <jona73eli@gmail.com>
2026-04-25 15:32:37 -0600

    feat: separacion de logica de IA en modulo spelling

---
+++ frontend/src/api/spelling.js
@@ -0,0 +1,10 @@
+
+/**
+ * @param {string} texto - Texto ingresado por el usuario
+ * @returns {Promise<string>} - Corrección de texto
+ */
+export const respuestaIA = (texto) => {
+    return new Promise((resolve) => {
+        resolve("Ta bien");
+    });
+}
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,4 @@
+import { respuestaIA } from './api/spelling';
 import './styles/main.css';
 
 const text = document.querySelector("#user-input"),
@@ -14,16 +15,33 @@ text.addEventListener("input", () => {
     }
 });
 
-buttonText.addEventListener("click", () => {
+buttonText.addEventListener("click", async() => {
+    // Mensaje del usuario
     let texto = text.value;
     const mensaje = document.createElement("div");
     mensaje.className = "message message--user";
     mensaje.innerHTML = `<p class = "message__bubble">${texto}</p>`;
     chat.appendChild(mensaje);
+
+    //Limpiar y desabilitar
     bienvenida.style.display = "none";
     text.value = '';
     buttonText.disabled = true;
-    respuestaIA();
+
+    //indicador de pensamiento
+    const typing= document.createElement("div");
+    typing.className = "message__typing";
+    typing.innerHTML = '<span class="message__typing-dot"></span>'.repeat(3);
+    chat.appendChild(typing);
+    text.disabled = true;
+    chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
+
+    const respuesta = await respuestaIA(texto)
+    //mostrar respuesta
+    typing.className = "message message--ai";
+    typing.innerHTML = `<p class = "message__bubble">${respuesta}</p>`;
+    chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
+    text.disabled = false;
 });
 
 text.addEventListener("keydown", (tecla) => {
@@ -36,18 +54,3 @@ text.addEventListener("keydown", (tecla) => {
     }
 } );
 
-const respuestaIA = ()=> {
-    const mensajeIA= document.createElement("div");
-    mensajeIA.className = "message__typing";
-    mensajeIA.innerHTML = '<span class="message__typing-dot"></span>'.repeat(3);
-    chat.appendChild(mensajeIA);
-    text.disabled = true;
-    chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
-    setTimeout(() => {
-        mensajeIA.innerHTML = "";
-        mensajeIA.className = "message message--ai";
-        mensajeIA.innerHTML = '<p class = "message__bubble">Ta bien</p>';
-        chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
-        text.disabled = false;
-    }, 3000);
-}
