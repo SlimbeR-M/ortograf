ddfab8d8a4ce9014d4c3f0a87a5c8b769c7d384e
Jonathan Hernández <jona73eli@gmail.com>
2026-04-26 12:44:48 -0600

    feat: conectar frontend con backend via fetch

--- frontend/src/api/spelling.js
+++ frontend/src/api/spelling.js
@@ -3,8 +3,17 @@
  * @param {string} texto - Texto ingresado por el usuario
  * @returns {Promise<string>} - Corrección de texto
  */
-export const respuestaIA = (texto) => {
-    return new Promise((resolve) => {
-        resolve("Ta bien");
+export const respuestaIA = async (texto) => {
+    const respuesta = await fetch("http://localhost:8000/corregir", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({texto: texto})
     });
+
+    const datos = await respuesta.json();
+
+    return datos.mensaje;
+
 }
