18eaa47c4be0d9c7d53c7457626b3267f8e8418c
Jonathan Hernández <jona73eli@gmail.com>
2026-04-25 19:47:39 -0600

    feat: boton de cancelar mensaje mientras IA responde

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,11 @@ const text = document.querySelector("#user-input"),
     buttonText = document.querySelector("#send-btn"),
     chat  = document.querySelector("#chat-messages"),
     bienvenida = document.querySelector("#welcome-screen"),
-    chatDisplay = document.querySelector("#chat-display");
+    chatDisplay = document.querySelector("#chat-display"),
+    icono = document.querySelector(".editor__send-icon");
+
+let esperandoRespuesta = false,
+    cancelado = false;
 
 text.addEventListener("input", () => {
     if(text.value.trim().length > 0) {
@@ -18,6 +22,18 @@ text.addEventListener("input", () => {
 });
 
 buttonText.addEventListener("click", async() => {
+    if(esperandoRespuesta) {
+        cancelado = true;
+        esperandoRespuesta = false;
+        icono.src = "/src/assets/enviar.png";
+        buttonText.disabled = true;
+        text.disabled = false;
+        return;
+    }
+
+    cancelado = false;
+    esperandoRespuesta = true;
+
     // Mensaje del usuario
     let texto = text.value;
     const mensaje = document.createElement("div");
@@ -28,7 +44,6 @@ buttonText.addEventListener("click", async() => {
     //Limpiar y desabilitar
     bienvenida.style.display = "none";
     text.value = '';
-    buttonText.disabled = true;
     text.style.height = "auto";
 
     //indicador de pensamiento
@@ -39,12 +54,27 @@ buttonText.addEventListener("click", async() => {
     text.disabled = true;
     chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
 
+    icono.src = "/src/assets/cancelar.png";
+    buttonText.disabled = false;
     const respuesta = await respuestaIA(texto)
+    if(cancelado) {
+        typing.remove();
+        esperandoRespuesta = false;
+        text.disabled = false;
+        return;
+    }
+
+
     //mostrar respuesta
     typing.className = "message message--ai";
     typing.innerHTML = `<p class = "message__bubble">${respuesta}</p>`;
     chatDisplay.scrollTo({ top: chatDisplay.scrollHeight, behavior: 'smooth' });
     text.disabled = false;
+    esperandoRespuesta = false;
+    buttonText.disabled = true;
+    icono.src = "/src/assets/enviar.png";
+
+
 });
 
 text.addEventListener("keydown", (tecla) => {
